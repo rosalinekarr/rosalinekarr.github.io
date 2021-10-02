e9ec89a614067669885fb4c0de1326f27408de12
Rose Karr <rose@rosalinekarr.com>
2021-10-02 15:22:44 -0400

    Updated styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,31 +3,32 @@ import "../main.css"
 
 export default function Home() {
   return (
-    <main className="vcard">
-      <div className="details">
-        <h1 className="n">
-          <span className="given-name">Rosaline</span>{" "}
-          <span className="additional-name">Annabelle</span>{" "}
-          <span className="family-name">Karr</span>
-        </h1>
-        <h2 className="category">Software Engineer</h2>
-        <a className="email" href="mailto:rose@rosalinekarr.com">
-          Email
-        </a>
-        <a href="/talks">
-          Talks
-        </a>
-        <a href="https://github.com/rosalinekarr">
-          GitHub
-        </a>
-        <a href="https://twitter.com/rosalinekarr">
-          Twitter
-        </a>
-        <a href="https://www.linkedin.com/in/rosalinekarr/">
-          LinkedIn
-        </a>
+    <main>
+      <div className="vcard">
+        <div className="details">
+          <h1 className="n">
+            <span className="given-name">Rosaline</span>{" "}
+            <span className="family-name">Karr</span>
+          </h1>
+          <h2 className="category">Software Engineer</h2>
+          <a className="email" href="mailto:rose@rosalinekarr.com">
+            Email
+          </a>
+          <a href="/talks">
+            Talks
+          </a>
+          <a href="https://github.com/rosalinekarr">
+            GitHub
+          </a>
+          <a href="https://twitter.com/rosalinekarr">
+            Twitter
+          </a>
+          <a href="https://www.linkedin.com/in/rosalinekarr/">
+            LinkedIn
+          </a>
+        </div>
+        <img className="photo" src="rose.jpg" alt="Rose" />
       </div>
-      <img className="photo" src="rose.png" alt="Rose" />
     </main>
   )
 }
